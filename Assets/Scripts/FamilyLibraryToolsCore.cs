a6ca0b8cd6294d5b8d0ae25bafe6f326e4f3c508
Seth - VirtusH <virtushda@gmail.com>
2021-01-29 01:04:13 -0500

    no message

---
+++ Assets/Scripts/FamilyLibraryToolsCore.cs
@@ -0,0 +1,46 @@
+﻿using FLT.IO;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace FLT
+{
+    public class FamilyLibraryToolsCore : MonoBehaviour
+    {
+        static FamilyLibraryToolsCore fltCore;
+        public static FamilyLibraryToolsCore FLTCore { get => fltCore; }
+
+        FLTReader reader;
+
+        public UnityEvent OnBaseFolderPathUpdate;
+        [ShowInInspector] string baseFolderPath;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            reader = new FLTReader();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SetFolderPath()
+        {
+            if (reader.PickFolder(out var newFolderPath))
+            {
+                baseFolderPath = newFolderPath;
+                OnBaseFolderPathUpdate?.Invoke();
+            }
+        }
+
+        public void Analyze()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/IO/FLTReader.cs
@@ -0,0 +1,34 @@
+﻿using SFB;
+using System.IO;
+
+namespace FLT.IO
+{
+    public class FLTReader
+    {
+        public bool PickFolder(out string newFolderPath, string startingPath = null)
+        {
+            string[] paths = StandaloneFileBrowser.OpenFolderPanel("Select a Folder", startingPath != null ? startingPath : "C:\\", false);
+
+            if (paths == null || paths.Length == 0)
+            {
+                newFolderPath = "";
+                return false;
+            }
+
+            if (Directory.Exists(paths[0]))
+            {
+                newFolderPath = paths[0];
+                return true;
+            }
+            else
+                newFolderPath = "";
+
+            return false;
+        }
+
+        public void AnalyzeFiles()
+        {
+            //Directory.
+        }
+    }
+}
